4fd774260147c05bd60dac431b69ba8ca1d55e0e
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-14 01:27:05 +0530

    added project to project list

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,19 @@ import { Fragment } from "react";
 import { GrGithub } from "react-icons/gr";
 import { RiExternalLinkFill } from "react-icons/ri";
 import { AiFillHtml5 } from "react-icons/ai";
-import { SiCss3, SiJavascript, SiReact, SiChakraui } from "react-icons/si";
+import { SiCss3, SiJavascript, SiReact, SiChakraui,SiExpress,SiMongodb } from "react-icons/si";
+import {IoLogoNodejs} from "react-icons/io"
 
-const [html, css, javascript, react, chakraui] = [
+
+const [html, css, javascript, react, chakraui,nodejs,expressjs,mongodb] = [
   <AiFillHtml5 size="2.2rem" color="#ff7b53e0" />,
-  <SiCss3 size="2rem" color="#ababff" />,
+  <SiCss3 size="2rem" color="#7b7bf7" />,
   <SiJavascript size="2rem" color="#3ca73c" />,
   <SiReact size="2rem" color="#61dafb" />,
   <SiChakraui size="2rem" color="#61dafb" />,
+  <IoLogoNodejs size="2rem" color="green" />,
+  <SiExpress size="2rem" color="#fff" />,
+<SiMongodb size="2rem" color="green" /> 
 ];
 
 const projects = [
@@ -49,6 +54,14 @@ const projects = [
     tech_stacks: [react, css, javascript],
     deployed_link: "https://google-clock-clone.netlify.app",
     github_link: "https://github.com/shreedharhegde99/Google-Timer-Clone",
+  },{
+    image:
+    "https://i.imgur.com/fiBFwiZ.png",
+    name: "Treasury Trip website",
+    desc: `Treasury trip is an website to expolre tourist places, book flight tickets,book airport taxis and book hotel rooms`,
+    tech_stacks: [react, chakraui, nodejs,expressjs,mongodb],
+    deployed_link: "https://treasury-trip.netlify.app/",
+    github_link: "https://github.com/shreedharhegde99/imported-existence-8823",
   },
 ];
 
@@ -61,13 +74,13 @@ function Projects() {
         </div>
 
         <div
-          className="flex flex-col gap-9 justify-center items-center  py-10  text-white 
+          className="flex flex-col gap-12 transition-all md:gap-9 justify-center items-center  py-10  text-white 
         md:flex-row md:flex-wrap md:items-stretch md:py-14  2xl:justify-start xl:px-10"
         >
           {projects.map((project) => (
             <div
               key={project.name}
-              className="bg-slate-500 rounded-md hover:bg-slate-800  hover:shadow-lg hover:shadow-cyan-500 hover:scale-105 transition-all
+              className="shadow-sm shadow-cyan-300 rounded-md bg-slate-800  hover:shadow-lg hover:shadow-cyan-500 hover:scale-105 transition-all
               max-w-lg flex flex-col items-center justify-center text-center p-2 md:w-max xl:my-4"
             >
               <div>
@@ -84,7 +97,8 @@ function Projects() {
                   </h1>
                   <p className="py-2">{project.desc}</p>
                 </div>
-                <div className=" py-4 flex justify-center gap-5">
+                <div className="text-xl">Tech Stacks</div>
+                <div className=" py-4 flex justify-center gap-5 ">
                   {project.tech_stacks.map((stack, i) => (
                     <div key={i}>{stack}</div>
                   ))}
