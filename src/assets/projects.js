78e4fce919824c83964b59e2388d289f1b859209
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-16 22:21:13 +0530

    moved projects data to asset folder

---
+++ src/assets/projects.js
@@ -0,0 +1,69 @@
+import { AiFillHtml5 } from "react-icons/ai";
+import {
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiChakraui,
+  SiExpress,
+  SiMongodb,
+} from "react-icons/si";
+import { IoLogoNodejs } from "react-icons/io";
+
+const [html, css, javascript, react, chakraui, nodejs, expressjs, mongodb] = [
+  <AiFillHtml5 size="2.2rem" color="#ff7b53e0" />,
+  <SiCss3 size="2rem" color="#7b7bf7" />,
+  <SiJavascript size="2rem" color="#3ca73c" />,
+  <SiReact size="2rem" color="#61dafb" />,
+  <SiChakraui size="2rem" color="#61dafb" />,
+  <IoLogoNodejs size="2rem" color="green" />,
+  <SiExpress size="2rem" color="#fff" />,
+  <SiMongodb size="2rem" color="green" />,
+];
+
+export const projectList = [
+  {
+    image: "https://i.imgur.com/1FA60E3.png",
+    name: "KFC Clone",
+    desc: `KFC is a fast food restaurant chain that specializes in fried chicken. 
+              It is the world's second-largest restaurant chain after McDonald's`,
+    tech_stacks: [html, css, javascript],
+    deployed_link: "https://kfc-clone-spry-smash-650.netlify.app/",
+    github_link: "https://github.com/shreedharhegde99/spry-smash-650",
+  },
+  {
+    image: "https://i.imgur.com/7t2lgxb.png",
+    name: "MyHours Clone",
+    desc: `MyHours is an online platform to organize projects, track time and report work.
+        It is good great tool to create great looking reports `,
+    tech_stacks: [react, chakraui],
+    deployed_link: "https://myhours.pages.dev/",
+    github_link: "https://github.com/shreedharhegde99/annoyed-spark-4097",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/shreedharhegde99/Envoy-Website-Clone/main/assets/Dashboard.png",
+    name: "Envoy Clone",
+    desc: `Envoy.com is an platform for reserving workspace,room and desks. 
+      It also provides services like visitor management,workspace protection and package pickup `,
+    tech_stacks: [html, css, javascript],
+    deployed_link: "https://envoy-website-clone.netlify.app/",
+    github_link: "https://github.com/shreedharhegde99/Envoy-Website-Clone",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/shreedharhegde99/Google-Timer-Clone/master/public/timer.png",
+    name: "Google Timer clone",
+    desc: `Google Timer is an app used to set countdown timer . We can also use this as a stopwatch `,
+    tech_stacks: [react, css, javascript],
+    deployed_link: "https://google-clock-clone.netlify.app",
+    github_link: "https://github.com/shreedharhegde99/Google-Timer-Clone",
+  },
+  {
+    image: "https://i.imgur.com/fiBFwiZ.png",
+    name: "Treasury Trip website",
+    desc: `Treasury trip is a website to explore tourist places, book flight tickets, airport taxis and hotel rooms`,
+    tech_stacks: [react, chakraui, nodejs, expressjs, mongodb],
+    deployed_link: "https://treasury-trip.netlify.app/",
+    github_link: "https://github.com/shreedharhegde99/imported-existence-8823",
+  },
+];
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,69 +1,7 @@
 import { Fragment } from "react";
 import { GrGithub } from "react-icons/gr";
 import { RiExternalLinkFill } from "react-icons/ri";
-import { AiFillHtml5 } from "react-icons/ai";
-import { SiCss3, SiJavascript, SiReact, SiChakraui,SiExpress,SiMongodb } from "react-icons/si";
-import {IoLogoNodejs} from "react-icons/io"
-
-
-const [html, css, javascript, react, chakraui,nodejs,expressjs,mongodb] = [
-  <AiFillHtml5 size="2.2rem" color="#ff7b53e0" />,
-  <SiCss3 size="2rem" color="#7b7bf7" />,
-  <SiJavascript size="2rem" color="#3ca73c" />,
-  <SiReact size="2rem" color="#61dafb" />,
-  <SiChakraui size="2rem" color="#61dafb" />,
-  <IoLogoNodejs size="2rem" color="green" />,
-  <SiExpress size="2rem" color="#fff" />,
-<SiMongodb size="2rem" color="green" /> 
-];
-
-const projects = [
-  {
-    image: "https://i.imgur.com/1FA60E3.png",
-    name: "KFC Clone",
-    desc: `KFC is a fast food restaurant chain that specializes in fried chicken. 
-            It is the world's second-largest restaurant chain after McDonald's`,
-    tech_stacks: [html, css, javascript],
-    deployed_link: "https://kfc-clone-spry-smash-650.netlify.app/",
-    github_link: "https://github.com/shreedharhegde99/spry-smash-650",
-  },
-  {
-    image: "https://i.imgur.com/7t2lgxb.png",
-    name: "MyHours Clone",
-    desc: `MyHours is an online platform to organize projects, track time and report work.
-      It is good great tool to create great looking reports `,
-    tech_stacks: [react, chakraui],
-    deployed_link: "https://myhours.pages.dev/",
-    github_link: "https://github.com/shreedharhegde99/annoyed-spark-4097",
-  },
-  {
-    image:
-      "https://raw.githubusercontent.com/shreedharhegde99/Envoy-Website-Clone/main/assets/Dashboard.png",
-    name: "Envoy Clone",
-    desc: `Envoy.com is an platform for reserving workspace,room and desks. 
-    It also provides services like visitor management,workspace protection and package pickup `,
-    tech_stacks: [html, css, javascript],
-    deployed_link: "https://envoy-website-clone.netlify.app/",
-    github_link: "https://github.com/shreedharhegde99/Envoy-Website-Clone",
-  },
-  {
-    image:
-      "https://raw.githubusercontent.com/shreedharhegde99/Google-Timer-Clone/master/public/timer.png",
-    name: "Google Timer clone",
-    desc: `Google Timer is an app used to set countdown timer . We can also use this as a stopwatch `,
-    tech_stacks: [react, css, javascript],
-    deployed_link: "https://google-clock-clone.netlify.app",
-    github_link: "https://github.com/shreedharhegde99/Google-Timer-Clone",
-  },{
-    image:
-    "https://i.imgur.com/fiBFwiZ.png",
-    name: "Treasury Trip website",
-    desc: `Treasury trip is a website to explore tourist places, book flight tickets, airport taxis and hotel rooms`,
-    tech_stacks: [react, chakraui, nodejs,expressjs,mongodb],
-    deployed_link: "https://treasury-trip.netlify.app/",
-    github_link: "https://github.com/shreedharhegde99/imported-existence-8823",
-  },
-];
+import { projectList } from "../assets/projects";
 
 function Projects() {
   return (
@@ -77,7 +15,7 @@ function Projects() {
           className="flex flex-col gap-12 transition-all md:gap-9 justify-center items-center  py-10  text-white 
         md:flex-row md:flex-wrap md:items-stretch md:py-14  2xl:justify-start xl:px-10"
         >
-          {projects.map((project) => (
+          {projectList.map((project) => (
             <div
               key={project.name}
               className="shadow-sm shadow-cyan-300 rounded-md bg-slate-800  hover:shadow-lg hover:shadow-cyan-500 hover:scale-105 transition-all
