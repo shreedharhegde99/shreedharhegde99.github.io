ae979fc8161625dbe93d56158a8750fcbbe1cbc1
Shreedhar Hegde <snhkelemane@gmail.com>
2022-11-21 17:02:33 +0530

    updated navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <Fragment>
-      <div className="h-24 flex p-4 items-center bg-slate-500 justify-end fixed w-full z-10 top-0">
+      <div className="h-24 flex p-4 items-center bg-gray-100  justify-end fixed w-full z-10 top-0">
         <div className="md:hidden">
           <div>
             <GiHamburgerMenu
@@ -18,27 +18,27 @@ export function Navbar() {
         </div>
 
         <div className="hidden justify-between text-2xl md:flex md:basis-2/3 lg:basis-3/5   xl:basis-1/3">
-          <div className="cursor-pointer border-b-2 border-slate-500  hover:text-white hover:border-b-2 hover:border-blue-500">
-            <a rel="noreferrer" href="#home">
-              Home
-            </a>
+          <div className="cursor-pointer py-1 px-2 border-b-4   border-gray-100  hover:text-white hover:bg-gray-200 hover:rounded-xl  hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-blue-500">
+            {/* <a rel="noreferrer" href="#home"> */}
+            Home
+            {/* </a> */}
           </div>
-          <div className="cursor-pointer border-b-2 border-slate-500 hover:text-white hover:border-b-2 hover:border-blue-500">
+          <div className="cursor-pointer py-1 px-2 border-b-4   border-gray-100 hover:text-white hover:bg-gray-200 hover:rounded-xl  hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-blue-500">
             <a rel="noreferrer" href="#techstack">
               Skills
             </a>
           </div>
-          <div className="cursor-pointer border-b-2 border-slate-500 hover:text-white hover:border-b-2 hover:border-blue-500">
+          <div className="cursor-pointer py-1 px-2 border-b-4   border-gray-100 hover:text-white hover:bg-gray-200 hover:rounded-xl  hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-blue-500">
             <a rel="noreferrer" href="#project">
               Project
             </a>
           </div>
-          <div className="cursor-pointer border-b-2 border-slate-500 hover:text-white hover:border-b-2 hover:border-blue-500">
+          <div className="cursor-pointer py-1 px-2 border-b-4   border-gray-100 hover:text-white hover:bg-gray-200 hover:rounded-xl  hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-blue-500">
             <a rel="noreferrer" href="#contact">
               Contact
             </a>
           </div>
-          <div className="cursor-pointer border-b-2 border-slate-500 hover:text-white hover:border-b-2 hover:border-blue-500">
+          <div className="cursor-pointer py-1 px-2 border-b-4   border-gray-100 hover:text-white hover:bg-gray-200 hover:rounded-xl  hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-blue-500">
             <a href="Resume.pdf" download="Shreedhar_Hegde_Resume.pdf">
               Resume
             </a>
