b4711ca1aa528268bb9ec1ae471f43fa6d066508
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-05 23:29:33 +0530

    modified github-stats card background color

--- src/components/GithubStats.jsx
+++ src/components/GithubStats.jsx
@@ -4,12 +4,12 @@ function GithubStats() {
   return (
     <div>
       <div className="bg-slate-500 p-2 py-4 ">
-        <h1 className="text-3xl text-white text-center">
+        <h1 className="text-3xl text-center text-yellow-300">
           Github Stats and Contribution
         </h1>
       </div>
       <div className="p-4 py-10 my-2 text-white ">
-        <div className="w-full flex justify-center py-8 ">
+        <div className="w-full flex justify-center py-8 text-lg ">
           <GitHubCalendar
             username="shreedharhegde99"
             year={new Date().getFullYear()}
@@ -18,14 +18,14 @@ function GithubStats() {
         <div className="py-8 flex flex-col items-center gap-5 lg:flex-row lg:items-center lg:justify-center">
           <a href="http://www.github.com/shreedharhegde99">
             <img
-              src="https://github-readme-stats.vercel.app/api?username=shreedharhegde99&show_icons=true&hide=&count_private=true&title_color=14b8a6&text_color=ffffff&icon_color=0891b2&bg_color=1c1917&hide_border=true&show_icons=true"
+              src="https://github-readme-stats.vercel.app/api?username=shreedharhegde99&show_icons=true&hide=&count_private=true&title_color=14b8a6&text_color=ffffff&icon_color=0891b2&bg_color=64748b&hide_border=true&show_icons=true"
               alt="shreedharhegde99's GitHub stats"
             />
           </a>
 
           <a href="http://www.github.com/shreedharhegde99">
             <img
-              src="https://github-readme-streak-stats.herokuapp.com/?user=shreedharhegde99&stroke=ffffff&background=1c1917&ring=14b8a6&fire=14b8a6&currStreakNum=ffffff&currStreakLabel=14b8a6&sideNums=ffffff&sideLabels=ffffff&dates=ffffff&hide_border=true"
+              src="https://github-readme-streak-stats.herokuapp.com/?user=shreedharhegde99&stroke=ffffff&background=64748b&ring=14b8a6&fire=14b8a6&currStreakNum=ffffff&currStreakLabel=14b8a6&sideNums=ffffff&sideLabels=ffffff&dates=ffffff&hide_border=true"
               alt="shreedharhegde99's GitHub contribution stats"
             />
           </a>
