cf5bbe81e56819f95af065ee81e9e8e158015b6b
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-10 15:09:42 +0530

    created heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+export default function Heading({ text }) {
+  return (
+    <div className="bg-violet-500/75 p-2 py-4 lg:mx-5">
+      <h1 className="text-3xl text-white text-center">{text}</h1>
+    </div>
+  );
+}
